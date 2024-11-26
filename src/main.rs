1b191fca4b15c6a4268b69b2ac8eb316b319efab
Jesse Na <jessena.dev@gmail.com>
2024-11-26 09:53:56 -0500

    Implement search and play functionality, and remove debugging code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-use std::sync::{Arc, Mutex};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
 
 use axum::{
-    debug_handler,
-    routing::{get, post}, Extension, Json, Router
+    extract::{Path, Query},
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Extension, Json, Router,
 };
-use kv::Store;
+use kv::{Codec, Store};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -20,6 +26,11 @@ struct Song {
     play_count: u32,
 }
 
+#[derive(Serialize)]
+struct SongNotFound<'a> {
+    error: &'a str,
+}
+
 #[tokio::main]
 async fn main() {
     let user_count = Arc::new(Mutex::new(0));
@@ -27,16 +38,21 @@ async fn main() {
     let store = Store::new(cfg).unwrap();
 
     let app = Router::new()
-        .route("/", get(|| async { "Welcome to the Rust-powered web server!" }))
-        .route("/count", get(|| async move {
-            let mut user_count = user_count.lock().unwrap();
-            *user_count += 1;
-            format!("Visit count: {}", *user_count)
-        }))
+        .route(
+            "/",
+            get(|| async { "Welcome to the Rust-powered web server!" }),
+        )
+        .route(
+            "/count",
+            get(|| async move {
+                let mut user_count = user_count.lock().unwrap();
+                *user_count += 1;
+                format!("Visit count: {}", *user_count)
+            }),
+        )
         .route("/songs/new", post(add_song))
-        // .route("/songs/search", get(|| async {
-        //     "Search for songs"
-        // }))
+        .route("/songs/search", get(search_song))
+        .route("/songs/play/:id", get(play_song))
         .layer(Extension(store));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
@@ -44,29 +60,110 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-#[debug_handler]
-async fn add_song(Extension(store): Extension<Store>, Json(payload): Json<Song>, ) -> String {
-    dbg!(&payload);
-
-    let db = store.bucket::<kv::Integer, kv::Json<Song>>(Some("songs")).unwrap();
+async fn add_song(Extension(store): Extension<Store>, Json(payload): Json<Song>) -> Json<Song> {
+    let db = store
+        .bucket::<kv::Integer, kv::Json<Song>>(Some("songs"))
+        .unwrap();
 
     let id = db.len() + 1;
+    let song = Song { id, ..payload };
 
-    let song = Song {
-        id,
-        ..payload
-    };
-
-    db.set(&kv::Integer::from(id), &kv::Json(song.clone())).unwrap();
-
+    db.set(&kv::Integer::from(id), &kv::Json(song.clone()))
+        .unwrap();
     db.flush_async().await.unwrap();
 
-    for item in db.iter() {
-        let item = item.unwrap();
-        let key: kv::Integer = item.key().unwrap();
-        let value = item.value::<kv::Json<Song>>().unwrap();
-        println!("key: {}, value: {}", usize::from(key), value);
-    }
+    Json(song)
+}
 
-    serde_json::to_string(&song).unwrap()
+async fn search_song(
+    Extension(store): Extension<Store>,
+    Query(params): Query<HashMap<String, String>>,
+) -> Json<Vec<Song>> {
+    let db = store
+        .bucket::<kv::Integer, kv::Json<Song>>(Some("songs"))
+        .unwrap();
+
+    let songs = db
+        .iter()
+        .filter_map(|item| {
+            let song = match item {
+                Ok(item) => match item.value::<kv::Json<Song>>() {
+                    Ok(song) => song.into_inner(),
+                    Err(_) => return None,
+                },
+                Err(_) => return None,
+            };
+
+            for (key, value) in &params {
+                match key.as_str() {
+                    "title" => {
+                        if !song
+                            .title
+                            .to_lowercase()
+                            .contains(value.to_lowercase().as_str())
+                        {
+                            return None;
+                        }
+                    }
+                    "artist" => {
+                        if !song
+                            .artist
+                            .to_lowercase()
+                            .contains(value.to_lowercase().as_str())
+                        {
+                            return None;
+                        }
+                    }
+                    "genre" => {
+                        if !song
+                            .genre
+                            .to_lowercase()
+                            .contains(value.to_lowercase().as_str())
+                        {
+                            return None;
+                        }
+                    }
+                    _ => {}
+                }
+            }
+
+            Some(song)
+        })
+        .collect::<Vec<Song>>();
+
+    Json(songs)
+}
+
+async fn play_song(
+    Extension(store): Extension<Store>,
+    Path(params): Path<HashMap<String, String>>,
+) -> impl IntoResponse {
+    const ERROR_JSON: Json<SongNotFound> = Json(SongNotFound {
+        error: "Song not found",
+    });
+
+    let db = store
+        .bucket::<kv::Integer, kv::Json<Song>>(Some("songs"))
+        .unwrap();
+
+    let id = match params.get("id") {
+        Some(id) => id.parse::<usize>().unwrap(),
+        None => return (StatusCode::NOT_FOUND, ERROR_JSON.into_response()),
+    };
+
+    let mut song = match db.get(&kv::Integer::from(id)) {
+        Ok(item) => match item {
+            Some(song) => song.into_inner(),
+            None => return (StatusCode::NOT_FOUND, ERROR_JSON.into_response()),
+        },
+        Err(_) => return (StatusCode::NOT_FOUND, ERROR_JSON.into_response()),
+    };
+
+    song.play_count += 1;
+
+    db.set(&kv::Integer::from(id), &kv::Json(song.clone()))
+        .unwrap();
+    db.flush_async().await.unwrap();
+
+    (StatusCode::OK, Json(song).into_response())
 }
